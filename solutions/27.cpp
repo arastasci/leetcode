2515c5d12428ba459022e90aa0459e0d96b14741
arastasci <arastasci@gmail.com>
2022-08-26 16:40:06 +0300

    initial commit

---
+++ solutions/27.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ind = 0;
+        for(int i = 0; i != nums.size(); i++){
+            if(nums[i] != val){
+                nums[ind++] = nums[i];
+            }
+        }
+        return ind;
+    }
+    // working with git
+};
