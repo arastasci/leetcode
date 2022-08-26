f3d78782d230621ae27fd737215f1e99ed30e7c6
arastasci <arastasci@gmail.com>
2022-08-26 16:08:19 +0300

    asdasd

---
+++ 242.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+    bool isAnagram(string s, string t){
+        vector<int> freq(128,0);
+        for(int i = 0; i < s.size(); i++) freq[s[i]]++;
+        for(int i = 0; i < t.size(); i++) freq[t[i]]--;
+        for (int i = 0; i != freq.size(); i++)
+            if (freq[i]) return false;
+        return true;
+    }
+};
