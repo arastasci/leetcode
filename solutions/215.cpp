81b9fc26d6fa7f5c9d2daed9c09732f220b3ad76
arastasci <arastasci@gmail.com>
2022-09-08 00:43:04 +0300

    49, 215

---
+++ solutions/215.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> heap;
+        for(int i = 0; i < nums.size(); i++){
+            heap.push(nums[i]);
+            if(heap.size() > k){
+                heap.pop();
+            }
+        }
+        return heap.top();
+    }
+};
---
+++ solutions/49.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<int>> counts;
+        unordered_map<string, vector<string>> keyvalPair;
+        for(int i=0; i< strs.size(); i++){
+            vector<int> count(26);
+            for(int j = 0; j < strs[i].size(); j++){
+                count[strs[i][j] -'a']++;
+            }
+            string x = "";
+            for(int j = 0; j < 26; j++){
+                x.append(to_string(count[j]+'a'));
+            }
+            keyvalPair[x].push_back(strs[i]);
+        }
+        vector<vector<string>> res;
+        for(auto& el = keyvalPair.begin(); el != keyvalPair.end(); el++){
+            res.push_back(el->second);
+        }
+        return res;
+        
+    }
+};
