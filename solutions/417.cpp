82636826f48a33240bcc412da632a5e10cd68cfc
arastasci <arastasci@gmail.com>
2022-09-15 18:36:52 +0300

    133,200,695 - notes not taken

---
+++ solutions/417.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        
+    }
+private:
+    pair<bool,bool> dfs(vector<vector<int>>& heights, int y, int x, int prevHeight, vector<vector<int>>& res){
+        
+    }
+};
